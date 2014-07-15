ca77357893f5168d0c7febde081d5989ef0b6e7f
Alex Neslusan <deadfoxygrandpa@gmail.com>
2014-07-15 14:34:51 +0800

    Remove nonworking export commands

--- elm-test-io.sh
+++ elm-test-io.sh
@@ -1,8 +1,5 @@
 #!/bin/bash
 
-export ELM_HOME="./elm-home"
-export ELM_IO="./ElmIO"
-
 sudo wget -P /usr/local/bin/ http://deadfoxygrandpa.github.io/elm-travis-cache/elm
 sudo wget -P /usr/local/bin/ http://deadfoxygrandpa.github.io/elm-travis-cache/elm-get
 sudo wget -P /usr/local/bin/ http://deadfoxygrandpa.github.io/elm-travis-cache/elm-io
