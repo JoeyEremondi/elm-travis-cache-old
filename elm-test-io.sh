3d69a9f769f74eb84a32ada4b5f0ed3b6a75d67e
Alex Neslusan <deadfoxygrandpa@gmail.com>
2014-07-15 16:08:10 +0800

    Update github account references

--- elm-test-io.sh
+++ elm-test-io.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
-sudo wget -P /usr/local/bin/ http://deadfoxygrandpa.github.io/elm-travis-cache/elm
-sudo wget -P /usr/local/bin/ http://deadfoxygrandpa.github.io/elm-travis-cache/elm-get
-sudo wget -P /usr/local/bin/ http://deadfoxygrandpa.github.io/elm-travis-cache/elm-io
-wget http://deadfoxygrandpa.github.io/elm-travis-cache/ElmIO.tar.gz
-wget http://deadfoxygrandpa.github.io/elm-travis-cache/elm_home.tar.gz
+sudo wget -P /usr/local/bin/ http://JoeyEremondi.github.io/elm-travis-cache/elm
+sudo wget -P /usr/local/bin/ http://JoeyEremondi.github.io/elm-travis-cache/elm-get
+sudo wget -P /usr/local/bin/ http://JoeyEremondi.github.io/elm-travis-cache/elm-io
+wget http://JoeyEremondi.github.io/elm-travis-cache/ElmIO.tar.gz
+wget http://JoeyEremondi.github.io/elm-travis-cache/elm_home.tar.gz
 sudo chmod +x /usr/local/bin/elm
 sudo chmod +x /usr/local/bin/elm-get
 sudo chmod +x /usr/local/bin/elm-io
